776f0462372464ad5bf688a3df291683a3daa495
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-10-16 16:31:12 -0500

    tag-routes.js findOne tag

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -29,7 +29,34 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id:req.params.id,
+    },
+  // be sure to include its associated Product data  
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name','stock','price', 'category_id'],
+        through: ProductTag,
+        as: 'products',
+      },
+    ],  
+  })
+    .then((dbTagData)=> {
+      if (!dbTagData){
+        res
+          .status(500)
+          .json({message:'No tag found'});
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch((err)=> {
+      console.log(err);
+      res.status(500).json(err);
+    });
+
 });
 
 router.post('/', (req, res) => {
