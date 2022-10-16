2ae3c7e981508c61b5ec4f1a58380e17f8187461
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-10-16 15:20:43 -0500

    api endpoints update category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -65,6 +65,27 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id:req.params.id,
+      },
+    }
+  )
+    .then((dbCategoryData)=> {
+      if (!dbCategoryData){
+        res.status(500).json({ message: 'No category found'});
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch ((err)=> {
+      console.log(err);
+      res.status(500).json (err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
