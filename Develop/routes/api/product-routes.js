155790c30a7b153ddebcaf36fcd2d0905fbe0065
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-10-16 15:57:32 -0500

    api endpoints productRoutes.js findAll products

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -6,7 +6,27 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: ['id','product_name','stock','price','category_id'],
+  // be sure to include its associated Category and Tag data  
+    include:[{
+        model:Category,
+        attributes:['id', 'category_name'],
+      },
+      {
+        model: Tag,
+        through: ProductTag,
+        as: 'tags',
+      },
+    ],
+  })
+    .then((dbProductData)=> res.json(dbProductData))
+    .catch((err)=> {
+      console.log(err)
+      res.status(500).json(err);
+
+    });
+
 });
 
 // get one product
