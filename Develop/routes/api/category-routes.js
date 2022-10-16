4d580894690607c8d48af92bc4a7732809128eb5
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-10-16 15:01:08 -0500

    api endpoints router.get category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -5,7 +5,21 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll({
+    attributes:['id','category_name'],
   // be sure to include its associated Products
+    include: [
+      {model: Product,
+      attributes: ['id','product_name','stock','price','category_id'],
+      },
+    ],
+  })
+    .then ((dbCategoryData)=> res.json(dbCategoryData))
+    .catch((err)=> {
+      console.log (err);
+      res.status(500).json(err);
+    });
+      
 });
 
 router.get('/:id', (req, res) => {
