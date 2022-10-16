f4a19f4956d81c5454d0f0c249e76d689d5da14c
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-10-16 15:24:47 -0500

    api endpoints delete category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -90,6 +90,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id:req.params.id
+    }
+  })
+    .then ((dbCategoryData)=> {
+      if(!dbCategoryData){
+        res.status(500).json({message: 'No category found'});
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log (err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
