0b8d221e248ba22e07adca4647f59b23924fdf05
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-10-16 16:05:04 -0500

    api endpoints product-routes.js findOne product

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -32,7 +32,35 @@ router.get('/', (req, res) => {
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findOne({
+    where:{
+      id:req.params.id,
+    },
   // be sure to include its associated Category and Tag data
+    include: [ {
+      model: Category,
+      attributes: ['id','category_name'],
+    },
+    {
+      model: Tag,
+      through: ProductTag,
+      as: 'tags',
+    },
+  ],
+  })
+    .then ((dbProductData)=> {
+      if(!dbProductData) {
+        res
+          .status(500)
+          .json({ message: 'No product found'});
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch((err)=> {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // create new product
